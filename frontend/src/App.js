a6fafbdf6c55f424f091814d64aef6400cae09ce
Matick973 <97759853+Matick973@users.noreply.github.com>
2023-02-24 02:43:20 +0100

    Suppression Redux
    Suppression de Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,11 @@ import Routes from './components/Routes';
 import { UserIdContext } from './components/AppContext';
 import { AdminContext } from './components/AppContext';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
 import { oneUser } from './feature/user.slice';
 
 
 function App() {
-  
-  const dispatch = useDispatch()
+
   const [ uid, setUid ] = useState(null)
   const [ isAdmin, setIsAdmin ] = useState(false)                 //Active les options d'administrateurs 
   
@@ -32,7 +30,7 @@ function App() {
             url: `${process.env.REACT_APP_API_URL}api/auth/user/${uid}`,
             withCredentials: true,
           })
-            .then((res) => { /*console.log(res);*/ dispatch(oneUser(res.data))
+            .then((res) => { /*console.log(res);*/
             
               sessionStorage.setItem("userID", res.data.User._id);
               sessionStorage.setItem("pseudo", res.data.User.pseudo);
